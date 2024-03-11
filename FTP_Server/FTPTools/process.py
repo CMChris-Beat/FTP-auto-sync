e395820263eb8823794d7c46bebb223388fc4503
樱落Eglo <452678184@qq.com>
2024-03-11 09:15:58 +0800

    添加了sync函数的释义注释

--- FTP_Server/FTPTools/process.py
+++ FTP_Server/FTPTools/process.py
@@ -2,6 +2,16 @@ from FTPTools import core
 
 
 def sync(host: str, port: int, username: str, password: str, FTP_DIR: str = None, BACKUP_DIR: str = None) -> None:
+    """
+    Sync FTP Server Files
+    :param host: 主机ip地址
+    :param port: 主机FTP服务端口号
+    :param username: 用户名
+    :param password: 密码
+    :param FTP_DIR: 本地FTP备份文件夹地址（绝对）
+    :param BACKUP_DIR: 本地修改或删除文件再备份文件夹地址（绝对）
+    :return: None
+    """
     sf = core.ScanFiles(
         host=host,
         port=port,
@@ -20,3 +30,7 @@ def sync(host: str, port: int, username: str, password: str, FTP_DIR: str = None
         sf.modify_file(datas['modify'])
     finally:
         sf.quit()
+
+
+def timing(time: str = "0:00:00") -> None:
+    pass
