a7950212eb63e83de3cc564fdc0f8c3e25d03ed8
樱落Eglo <452678184@qq.com>
2024-04-07 09:51:18 +0800

    修复了json文件包含中文报编码错误问题 #18

--- FTP_Server/main.py
+++ FTP_Server/main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 
 if __name__ == '__main__':
-    config = json.load(open('./config.json', encoding='utf-8'))
+    config = json.load(open('./config.json', encoding='gbk'))
     config['LOCAL_BACKUP_DIR'] = None if config['LOCAL_BACKUP_DIR'] == '' else config['LOCAL_BACKUP_DIR']
     config['RE_BACKUP_DIR'] = None if config['RE_BACKUP_DIR'] == '' else config['RE_BACKUP_DIR']
     config['FTP_SERVER_DIR'] = None if config['FTP_SERVER_DIR'] == '' else config['FTP_SERVER_DIR']
